81c23f127a6a4945e8cff84a40cf099bc39b9c89
gabby <hannahfrench1709@hotmail.com>
2025-09-25 18:40:37 +0100

    chore: set up server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,20 @@
-// set up a server 
+import express from "express";
+import cors from "cors";
+import { db } from "./database-connection.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
+
+app.get("/", (request, response) => {
+    response.json({ message: "Welcome to the server. GET comfy!" });
+});
 
 // todo - want a route to READ data
 
