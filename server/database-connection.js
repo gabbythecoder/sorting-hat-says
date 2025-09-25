d1205f5ec98460cae859f08ca0a5e92d6a78f6ca
gabby <hannahfrench1709@hotmail.com>
2025-09-25 18:39:48 +0100

    chore: set up database-connection

--- server/database-connection.js
+++ server/database-connection.js
@@ -1,1 +1,10 @@
-// todo - set up the database pool 
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const databaseConnectionString = process.env.DB_URL;
+
+export const db = new pg.Pool({
+    connectionString: databaseConnectionString,
+});
