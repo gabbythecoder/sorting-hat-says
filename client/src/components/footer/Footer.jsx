0db32a81471bd8f8c3c43bd0379daf4ebfbafd1e
gabby <hannahfrench1709@hotmail.com>
2025-09-26 18:24:03 +0100

    style: add styling to Footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,8 +2,9 @@ import "./Footer.css";
 
 export default function Footer() {
     return (
-        <footer>
-            <p>&copy; Gabby French | Made with ❤️ in the UK</p>
+        <footer className="footer-container">
+            <p className="footer-text">&copy; Gabby French | Made with ❤️ in the UK</p>
+            <p className="footer-text">Disclaimer: This site is a student project for educational use only. All franchise materials are owned by their copyright holders. No infringement intended.</p>
         </footer>
     )
 }
