d166acfe9c1233146bb9a65bcf29f57a62effecd
gabby <hannahfrench1709@hotmail.com>
2025-09-26 23:09:08 +0100

    feat: add shorter disclaimer for mobile view

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
     return (
         <footer className="footer-container">
             <p className="footer-text">&copy; Gabby French | Made with ❤️ in the UK</p>
-            <p className="footer-text">Disclaimer: This site is a student project for educational use only. All franchise materials are owned by their copyright holders. No infringement intended.</p>
+            <p className="footer-text-desktop">Disclaimer: This site is a student project for educational use only. All franchise materials are owned by their copyright holders. No infringement intended.</p>
+            <p className="footer-text-mobile">Disclaimer: Student project for educational use only. All rights belong to their owners.</p>
         </footer>
     )
 }
