ec5887ebd1cff95a97f10165ed5ea6624416e38b
gabby <hannahfrench1709@hotmail.com>
2025-09-26 18:23:11 +0100

    style: add tailwindcss in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,15 +9,18 @@ import Footer from "./components/footer/Footer";
 export default function App() {
     return (
         <>
+        <div className="flex flex-col min-h-screen">
             <Header />
 
+        <main className="flex-1">
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/form" element={<Form />} />
         
         </Routes>
-
+        </main>
             <Footer />
+        </div>
         </>
     )
 }
