f265e115fef1cf8e5c75ea60a99edc8f380867c3
gabby <hannahfrench1709@hotmail.com>
2025-09-26 15:41:34 +0100

    feat: add recentlysortedusers folder for RecentlySortedUsers component

---
+++ client/src/components/recentlysortedusers/RecentlySortedUsers.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import "./RecentlySortedUsers.css";
+
+export default function RecentlySortedUsers() {
+    const [quizResult, setQuizResult] = useState([]);
+
+    useEffect(() => {
+        async function getQuizResultData() {
+            try {
+                const response = await fetch("http://localhost:8080/quiz-result");
+                const data = await response.json();
+                setQuizResult(data);
+            } catch (error) {
+                console.error("Failed to fetch API", error);
+            }
+        } 
+        getQuizResultData();
+
+        const quizResultInterval = setInterval(getQuizResultData, 3000);
+        return () => clearInterval(quizResultInterval);
+    }, []);
+
+    return (
+        <>
+            <div>
+                <h3>Recently Sorted Users</h3>
+                <div className="user-cards-container">
+                    {quizResult.map((user, index) => (
+                        <div className="user-cards" key={`${user.user_name}-${index}`}>
+                            <p>{user.user_name}</p>
+                            <p>{user.sorting_house.charAt(0).toUpperCase() + user.sorting_house.slice(1)}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
