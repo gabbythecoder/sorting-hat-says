3ff9286bb376cfe75d5f9c59be3bcfed31a7ca56
gabby <hannahfrench1709@hotmail.com>
2025-09-26 15:40:03 +0100

    feat: add result folder for Result component

---
+++ client/src/components/result/Result.jsx
@@ -0,0 +1,19 @@
+import houseDescriptions from "../../lib/houseDescriptions";
+
+export default function Result({ house }) {
+    const info = house ? houseDescriptions[house.toLowerCase()] : undefined;
+
+    return (
+        <>
+        {info ? ( 
+            <div className="result-container">
+                <h2>You're in {info.name}!</h2>
+                <p>Traits: {info.traits}</p>
+                <p>{info.description}</p>
+            </div>
+            ) : (
+                <p>Oops! We couldn't determine your house.</p>
+            )}
+        </>
+    )
+}
