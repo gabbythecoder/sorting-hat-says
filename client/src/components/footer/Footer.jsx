a449b1bf688ae6881894e0da8bcd4488491bef3a
gabby <hannahfrench1709@hotmail.com>
2025-09-26 11:53:54 +0100

    feat: add footer folder for Footer component

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>&copy; Gabby French | Made with ❤️ in the UK</p>
+        </footer>
+    )
+}
