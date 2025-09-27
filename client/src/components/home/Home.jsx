40f6eb613ee0e0151ff15cbabc3c2834fffd0f50
gabby <hannahfrench1709@hotmail.com>
2025-09-27 13:16:21 +0100

    style: add styling to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router";
+import "./Home.css";
 
 export default function Home() {
     return (
         <>
-            <h1>Welcome</h1>
-
-            <Link to={"form"}>Quiz</Link>
+            <div className="home-page-container">
+                <h2 className="home-page-title">SORTING HAT SAYS</h2>
+                    <button className="quiz-button">
+                        <Link to={"quiz"}>TAKE THE QUIZ</Link>
+                    </button>
+            </div>
         </>
     )
 }
